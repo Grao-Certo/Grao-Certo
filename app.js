cfd6a2ad6f5318654fbb569f4ffce23c49b4b55e
PedroLima7011 <pedro.dlima@sptech.school>
2026-05-21 19:47:14 -0300

    Feat: Adiciona o Back-end para inserção de dados da página cadastrar silo

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var app = express();
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
+var siloRouter = require("./src/routes/silo");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,6 +24,7 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
+app.use("/silo", siloRouter);
 
 
 app.listen(PORTA_APP, function () {
