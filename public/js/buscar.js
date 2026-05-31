3cb1da431eaa4c06d119e83214781fa104b09a75
VMBB-sptech <victor.bertolino@sptech.school>
2026-05-31 00:55:17 -0300

    fix: removendo páginas obsoletas e concertando destaque na listagem dinamica

--- public/js/buscar.js
+++ public/js/buscar.js
@@ -16,22 +16,36 @@ function buscarSilos(){
                 if(resposta.ok){
                     resposta.json().then(dados => {       
                         if(dados.length > 0){
+                            //buscando a página atual + o id do silo
+                            let paginaAtual = window.location.pathname;
+                            let siloAtual = sessionStorage.getItem("ID_SILO_ATUAL");
+
+                            //validando se a página atual é a dashboard geral, caso seja limpa a tag active ou coloca ela
+                            let ativoAvisos = paginaAtual.includes("dashboardGeral.html") ? 'id="active"' : '';
                             let admin = validarAdmin();
 
                             if(admin){
                                 mensagem += `
-                                <li> <a href="dashboardGeral.html" id="active">AVISOS</a> </li>
+                                <li> <a href="dashboardGeral.html" ${ativoAvisos}>AVISOS</a> </li>
                                 <li> <a href="cadastroSilos.html">CADASTRAR NOVO SILO</a> </li>
                                 <li> <a href="cadastroUsuario.html">CADASTRAR NOVO USUÁRIO</a> </li>
                                 `;
                             }else{
                                 mensagem += `
-                                <li> <a href="dashboardGeral.html" id="active">AVISOS</a> </li>
+                                <li> <a href="dashboardGeral.html" ${ativoAvisos}>AVISOS</a> </li>
                                 `;
                             }
 
                             for(let i = 0; i < dados.length; i++){
-                                mensagem += `<li> <a onclick="acessarSilo(${dados[i].id})"> SILO ${i + 1}</a></li>`;
+                                let ativoSilo = '';
+
+                                // Se estiver na dashboardSilos e o ID do silo atual for igual, deixa verde
+                                if (paginaAtual.includes("dashboardSilos.html") && dados[i].id == siloAtual) {
+                                    ativoSilo = 'id="active"';
+                                }
+
+                                mensagem += `<li> <a onclick="acessarSilo(${dados[i].id})" ${ativoSilo}> SILO ${i + 1}</a></li>`;
+
                             }
                             ul_submenu.innerHTML = mensagem;
                         }else{
