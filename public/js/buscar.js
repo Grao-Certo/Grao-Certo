f8d7954dabac77af07a34aae09a487cf66da26f5
VMBB-sptech <victor.bertolino@sptech.school>
2026-05-31 00:24:41 -0300

    feat: aprimorando a leitura de código e iniciando a implementação de redirect dinamico nos silos

--- public/js/buscar.js
+++ public/js/buscar.js
@@ -1,53 +1,57 @@
 function buscarSilos(){
     let id = sessionStorage.ID_USUARIO;
 
-    fetch(`/silo/buscarSilos/${id}`, {
-                                method: "GET",
-                                headers: {
-                                    "Content-Type": "application/json",
-                                },
-                             
-                            })
-                                .then(function (resposta) {
-                                    console.log("resposta: ", resposta);
-                                    let mensagem = '';
+    fetch(`/silo/buscarSilos/${id}`,
+        {
+            method: "GET",
+            headers: {
+            "Content-Type": "application/json",
+            },
+
+        }).then(
+            function (resposta) {
+                console.log("resposta: ", resposta);
+                let mensagem = '';
                                     
-                                    if(resposta.ok){
-                                        resposta.json().then(dados => {       
-                                            if(dados.length > 0){
+                if(resposta.ok){
+                    resposta.json().then(dados => {       
+                        if(dados.length > 0){
+                            let admin = validarAdmin();
 
-                                                let admin = validarAdmin();
-                                                
-                                                if(admin){
-                                                    mensagem += `
-                                                    <li> <a href="dashboardGeral.html" id="active">AVISOS</a> </li>
-                                                    <li> <a href="cadastroSilos.html">CADASTRAR NOVO SILO</a> </li>
-                                                    <li> <a href="cadastroUsuario.html">CADASTRAR NOVO USUÁRIO</a> </li>
-                                                    `;
-                                                }else{
-                                                    mensagem += `
-                                                    <li> <a href="dashboardGeral.html" id="active">AVISOS</a> </li>
-                                                    `;
-                                                }
-            
-                                                    for(let i = 0; i < dados.length; i++){
-                                                        mensagem += `<li> <a href="dashboardSilo/${dados[i].id}.html">SILO ${i + 1}</a></li>`;
-                                                    }
-            
-            
-                                                    ul_submenu.innerHTML = mensagem;
-                                            }else{
-                                                mensagem += `
-                                                <li> <a href="dashboardGeral.html" id="active">AVISOS</a> </li>
-                                                `;
-        
-                                                ul_submenu.innerHTML = mensagem;
-                                            }
-                                        });
-                                    }
+                            if(admin){
+                                mensagem += `
+                                <li> <a href="dashboardGeral.html" id="active">AVISOS</a> </li>
+                                <li> <a href="cadastroSilos.html">CADASTRAR NOVO SILO</a> </li>
+                                <li> <a href="cadastroUsuario.html">CADASTRAR NOVO USUÁRIO</a> </li>
+                                `;
+                            }else{
+                                mensagem += `
+                                <li> <a href="dashboardGeral.html" id="active">AVISOS</a> </li>
+                                `;
+                            }
 
-                                })
-                                .catch(function (resposta) {
-                                    console.log(`#ERRO: ${resposta}`);
-                                });
+                            for(let i = 0; i < dados.length; i++){
+                                mensagem += `<li> <a onclick="acessarSilo(${dados[i].id})"> SILO ${i + 1}</a></li>`;
+                            }
+                            ul_submenu.innerHTML = mensagem;
+                        }else{
+                            mensagem += `
+                            <li> <a href="dashboardGeral.html" id="active">AVISOS</a> </li>
+                            `;
+                            ul_submenu.innerHTML = mensagem;
+                        }
+                    });
+                }
+            }
+        ).catch(
+            function (resposta) {
+                console.log(`#ERRO: ${resposta}`);
+            }
+        );
+}
+
+function acessarSilo(idSilo) {
+    sessionStorage.setItem("ID_SILO_ATUAL", idSilo);
+    
+    window.location.href = "dashboardSilos.html";
 }
