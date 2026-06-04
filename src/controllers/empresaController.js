13c84738a40b9efa963235e7c0058b8e3b88b7a2
VMBB-sptech <victor.bertolino@sptech.school>
2026-06-04 01:53:33 -0300

    fix: movimentando o buscarMovimentacaoSemanal de empresaController para SiloController devido a implementação incorreta

--- src/controllers/empresaController.js
+++ src/controllers/empresaController.js
@@ -53,29 +53,9 @@ function cadastrar(req, res) {
     }
 }
 
-function buscarMovimentacaoSemanal(req, res) {
-    var idSilo = req.params.idSilo;
-
-    if (idSilo == undefined) {
-        res.status(400).send("O id do silo está indefinido!");
-    } else {
-        siloModel.buscarMovimentacaoSemanal(idSilo).then(
-            function (resultado) {
-                res.status(200).json(resultado);
-            }
-        ).catch(
-            function (erro) {
-                console.log(erro);
-                res.status(500).json(erro.sqlMessage);
-            }
-        );
-    }
-}
-
 module.exports = {
     buscarPorCnpj,
     buscarPorId,
     cadastrar,
-    listar,
-    buscarMovimentacaoSemanal
+    listar
 };
--- src/controllers/siloController.js
+++ src/controllers/siloController.js
@@ -144,11 +144,31 @@ function buscarVolumeMensal(req, res) {
     }
 }
 
+function buscarMovimentacaoSemanal(req, res) {
+    var idSilo = req.params.idSilo;
+
+    if (idSilo == undefined) {
+        res.status(400).send("O id do silo está indefinido!");
+    } else {
+        siloModel.buscarMovimentacaoSemanal(idSilo).then(
+            function (resultado) {
+                res.status(200).json(resultado);
+            }
+        ).catch(
+            function (erro) {
+                console.log(erro);
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+    }
+}
+
 module.exports = {
     cadastrarSilo,
     buscarSilos,
     obterTotalSilos,
     obterDadosGerais,
     buscarMedidaMaisRecente,
-    buscarVolumeMensal
+    buscarVolumeMensal,
+    buscarMovimentacaoSemanal
 }
